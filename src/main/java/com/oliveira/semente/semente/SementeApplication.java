0afcbd88a13ebceba73650abd1ae249742977faf
Heitor Carneiro <heitorgcarneiro@gmail.com>
2018-10-13 18:32:53 -0300

    [heroku] - Criando aplicacao com SpringBoot para subir no heroku

---
+++ src/main/java/com/oliveira/semente/semente/SementeApplication.java
@@ -0,0 +1,12 @@
+package com.oliveira.semente.semente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SementeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SementeApplication.class, args);
+	}
+}
